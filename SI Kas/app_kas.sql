5a576e6c9b7a531a8246c9dbbb362f1447c92407
RizkiAuliaRamadhan <rwamdhaney2@gmail.com>
2020-12-19 13:40:19 +0700

    Aplikasi SI Kas

--- SI Kas/app_kas.sql
+++ SI Kas/app_kas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 14 Des 2020 pada 14.24
+-- Waktu pembuatan: 19 Des 2020 pada 07.38
 -- Versi server: 10.4.13-MariaDB
 -- Versi PHP: 7.4.7
 
@@ -42,14 +42,16 @@ INSERT INTO `pemasukan` (`id_pemasukan`, `nominal`, `keterangan`, `tanggal_pemas
 (17, 500000, 'cek', '2020-01-01'),
 (18, 300000, 'cek', '2020-09-01'),
 (19, 300000, 'cek\r\n', '2020-02-04'),
-(21, 100000, '', '2020-03-03'),
-(22, 900000, '', '2020-10-01'),
+(21, 100000, 'cek', '2020-03-03'),
+(22, 900000, 'cek', '2020-10-01'),
 (23, 200000, 'cek', '2020-05-02'),
 (24, 200000, 'cek', '2020-04-11'),
 (25, 100000, 'cek', '2020-06-23'),
 (26, 50000, 'cek', '2020-07-31'),
 (27, 450000, 'cek', '2020-08-12'),
-(28, 100000, 'cek', '2020-12-14');
+(28, 100000, 'cek', '2020-12-14'),
+(29, 100000, 'cek', '2019-01-01'),
+(30, 200000, 'cek', '2018-01-01');
 
 -- --------------------------------------------------------
 
@@ -72,7 +74,8 @@ INSERT INTO `pengeluaran` (`id_pengeluaran`, `nominal`, `keterangan`, `tanggal_p
 (1, 700000, 'cek', '2020-11-27'),
 (4, 200000, 'cek', '2020-12-01'),
 (5, 1000000, 'cek', '2020-12-04'),
-(6, 200000, 'cek', '2020-12-14');
+(6, 200000, 'cek', '2020-12-14'),
+(7, 100000, 'cek', '2019-01-01');
 
 -- --------------------------------------------------------
 
@@ -93,7 +96,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `username`, `password`, `level`) VALUES
 (4, 'admin', '$2y$10$52tA3t9nfyffyPwL75LOvuXlvOYCLKKHomXlw2XYnnMEX58as5J/K', 'admin'),
-(18, 'rama', '$2y$10$OAiBkt22dBOrojti3QeAK.Dm9ojKzp5qTDS9wdHQwGUdIJNvdZc0S', 'user');
+(18, 'rama', '$2y$10$OAiBkt22dBOrojti3QeAK.Dm9ojKzp5qTDS9wdHQwGUdIJNvdZc0S', 'user'),
+(20, 'user', '$2y$10$7Q6JgYh5WHxyJ2DvdDnkAeEKUlY0PL0dx/DTFJvX2IRbtN/vFDP8e', 'user');
 
 --
 -- Indexes for dumped tables
@@ -125,19 +129,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `pemasukan`
 --
 ALTER TABLE `pemasukan`
-  MODIFY `id_pemasukan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id_pemasukan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengeluaran`
 --
 ALTER TABLE `pengeluaran`
-  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
